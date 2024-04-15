71980ae679cf62f226fc3d913b1c002f55c7ed3b
Manaure Vasconcelos <manaurevasc97@gmail.com>
2024-04-15 17:50:11 -0300

    New branch

--- src/interfaces/ingredient-protocol.ts
+++
@@ -1,8 +0,0 @@
-export interface IngredientProtocol {
-  describe: string;
-  marketWeight: number;
-  marketPrice: number;
-  grossWeight: number;
-  _realAmount?: number;
-  setRealAmount(): void;
-}
--- src/table-cost-unit/table-cost-unit.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TableCostUnitController } from './table-cost-unit.controller';
-import { TableCostUnitService } from './table-cost-unit.service';
-
-@Module({
-  controllers: [TableCostUnitController],
-  providers: [TableCostUnitService],
-})
-export class TableCostUnitModule {}
