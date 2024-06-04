5d4359c73cd74da7818292e7c0dfc396debef8a6
Manaure Vasconcelos <manaurevasc97@gmail.com>
2024-06-04 20:51:22 -0300

    Feat/test: create Recipe entity and test

---
+++ src/application/entities/recipe.entity.spec.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it } from 'vitest';
+import { RecipeEntity } from './recipe.entity';
+
+describe('Recipe Entity', () => {
+  it('should be able create Recipe', () => {
+    const recipe = new RecipeEntity({
+      title: 'test',
+      userId: 'test',
+    });
+
+    expect(recipe).toBeTruthy();
+  });
+
+  it('should be able create a new instancy of Recipe', () => {
+    const recipe = new RecipeEntity({
+      title: 'test',
+      userId: 'test',
+    });
+
+    const recipe2 = new RecipeEntity({
+      id: recipe.id,
+      title: recipe.title,
+      describe: recipe.describe,
+      userId: recipe.userId,
+      createAt: recipe.createAt,
+      valuePartial: recipe.valuePartial,
+    });
+
+    expect(recipe2.id).toBe('fakeId');
+  });
+});
--- src/application/entities/recipe.entity.ts
+++ src/application/entities/recipe.entity.ts
@@ -1,16 +1,69 @@
-import { randomUUID } from 'node:crypto';
+import { Replace } from '@helpers/Replace';
+
+interface RecipeProps {
+  id: string;
+  title: string;
+  describe: string | null;
+  userId: string;
+  createAt: Date;
+  valuePartial: number | null;
+}
 
 export class RecipeEntity {
-  private readonly id: string = randomUUID();
-  private valuePartial?: number;
+  private props: RecipeProps;
 
   constructor(
-    private title: string,
-    private describe: string,
-    private userId: number,
-  ) {}
+    props: Replace<
+      RecipeProps,
+      { id?: string; describe?: string; createAt?: Date; valuePartial?: number }
+    >,
+  ) {
+    this.props = {
+      id: props.id ?? 'fakeId',
+      ...props,
+      describe: props.describe ?? null,
+      createAt: props.createAt ?? new Date(),
+      valuePartial: props.valuePartial ?? null,
+    };
+  }
 
-  setValuePartial(value: number) {
-    this.valuePartial = value;
+  set id(id: string) {
+    this.props.id = id;
+  }
+
+  get id(): string {
+    return this.props.id;
+  }
+
+  set title(title: string) {
+    this.props.title = title;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  set describe(describe: string) {
+    this.props.describe = describe;
+  }
+
+  get describe(): string | null {
+    return this.props.describe;
+  }
+
+  set valuePartial(valuePartial: number) {
+    this.props.valuePartial = valuePartial;
+  }
+
+  get valuePartial(): number | null {
+    return this.props.valuePartial;
+  }
+
+  get userId() {
+    return this.props.userId;
+  }
+
+  get createAt(): Date {
+    return this.props.createAt;
   }
 }
